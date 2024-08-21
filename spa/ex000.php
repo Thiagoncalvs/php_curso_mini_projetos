f820bcf344d96e09ad1cecbb9f7b88fca98fabe4
THIAGO S. GONÇALVES <thiago_gub@hotmail.com>
2024-08-20 23:26:50 -0300

    Última aula feita Nº17

---
+++ spa/ex000.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ex000</title>
+</head>
+<body>
+    <nav><li><ul><a href="/">HOME</a></ul></li></nav>
+
+        <h1><?php echo "Olá, mundo, com echo! ";?></h1>
+
+        <h1><?php print " Olá, mundo, com print!";?></h1>
+
+        <h1><?php echo "\u{1F44B}, \u{1F30E}, com unicode";?></h1>
+</body>
+</html>
---
+++ spa/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <nav>
+        <li>
+            <ul><a href="/">HOME</a></ul>
+            <ul><a href="/ex000.php">ex000</a></ul>
+            <ul><a href="/ex001.php">ex001</a></ul>
+            <ul><a href="/ex002.php">ex002</a></ul>
+            <ul><a href="/ex003.php">ex003</a></ul>
+            <ul><a href="/ex004.php">ex004</a></ul>
+        </li>
+    </nav>
+</body>
+</html>
